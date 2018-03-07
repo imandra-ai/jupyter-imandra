f75e5b7625d5a506641da4c62fd3ddd1c589b5ce
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-03-07 16:15:11 -0600

    detail

--- src/jymandra.ml
+++ src/jymandra.ml
@@ -66,7 +66,9 @@ let inspect (r:C.Kernel.inspect_request) : (C.Kernel.inspect_reply_ok, string) r
         Document.to_string @@ History.event_to_doc ~txt:true ev
       and html =
         Doc_render.mime_of_html @@ Doc_render.to_html @@
-        History.event_to_doc ~txt:false ev
+        History.event_to_doc ~txt:true ev
+        (* FIXME: use once we have a good rendering of terms in HTML
+           History.event_to_doc ~txt:true ev *)
       in
       Ok {C.Kernel.iro_status="ok"; iro_found=true; iro_data=[txt;html]}
   with e ->
