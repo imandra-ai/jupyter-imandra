2eaf5590246dbd86347085f4065110cb67017948
Dave Aitken <dave.aitken@gmail.com>
2018-05-03 11:43:48 +0100

    Include full constraints for each region group

--- src/Decompose_render.ml
+++ src/Decompose_render.ml
@@ -10,21 +10,21 @@ module J = Yojson.Basic
 module StringMap = CCMap.Make(String)
 
 type region =
-  { constraints : string list
-  ; invariant : string
+  { r_constraints : string list
+  ; r_invariant : string
   }
 
 type region_group =
-  { konstraint: string
-  ; label_path : int list
-  ; region: region option
-  ; children : region_group list
-  ; weight: int
+  { rg_constraints: string list
+  ; rg_label_path : int list
+  ; rg_region: region option
+  ; rg_children : region_group list
+  ; rg_weight: int
   }
 
 let to_region (decompose_region : Top_result.decompose_region) : region =
-  { constraints = CCList.map Term.to_string decompose_region.reg_constraints
-  ; invariant = Term.to_string decompose_region.reg_invariant
+  { r_constraints = CCList.map Term.to_string decompose_region.reg_constraints
+  ; r_invariant = Term.to_string decompose_region.reg_invariant
   }
 
 module StringSet = CCSet.Make(String)
@@ -33,7 +33,7 @@ let rec group_regions (idx_path : int list) (constraint_path: string list) (regi
   let open Top_result in
   let all_constraints_with_dup =
     regions
-    |> CCList.flat_map (fun r -> r.constraints)
+    |> CCList.flat_map (fun r -> r.r_constraints)
     |> CCList.filter (fun c1 -> not (CCList.exists (fun c2 -> c1 = c2) constraint_path) )
   in
   let constraints_by_most_frequent =
@@ -50,17 +50,18 @@ let rec group_regions (idx_path : int list) (constraint_path: string list) (regi
     constraints_by_most_frequent
     |> CCList.fold_left (fun (groups, regions) konstraint ->
         let (has, without) = regions |> CCList.partition (fun r ->
-            CCList.exists (fun c -> c = konstraint) r.constraints) in
+            CCList.exists (fun c -> c = konstraint) r.r_constraints) in
 
         let i = CCList.length groups + 1 in
-        let label_path = i :: idx_path in
+        let idx_path = i :: idx_path in
+        let constraint_path = konstraint :: constraint_path in
 
         (if CCList.length has > 0 then
-           ({ konstraint = konstraint
-            ; region = if CCList.length has = 1 then Some (List.hd has) else None
-            ; children = group_regions label_path (konstraint :: constraint_path) has
-            ; label_path = label_path
-            ; weight = CCList.length has
+           ({ rg_constraints = (CCList.rev constraint_path)
+            ; rg_region = if CCList.length has = 1 then Some (List.hd has) else None
+            ; rg_children = group_regions idx_path (constraint_path) has
+            ; rg_label_path = idx_path
+            ; rg_weight = CCList.length has
             } :: groups
            , without)
          else
@@ -72,18 +73,18 @@ let rec group_regions (idx_path : int list) (constraint_path: string list) (regi
   grouped |> fst
 
 let region_to_json (r : region) : J.json =
-  `Assoc [ ("constraints", `List (CCList.map (fun c -> `String c) r.constraints))
-         ; ("invariant", `String r.invariant)
+  `Assoc [ ("constraints", `List (CCList.map (fun c -> `String c) r.r_constraints))
+         ; ("invariant", `String r.r_invariant)
          ]
 
 let rec region_group_to_json (rg : region_group) : J.json =
-  let label_path = (rg.label_path |> CCList.rev |> CCList.map string_of_int |> CCString.concat ".") in
-  let label = match rg.region with Some r -> "@ " ^ label_path | None -> label_path in
-  `Assoc [ ("constraint", `String rg.konstraint)
-         ; ("region", match rg.region with Some r -> region_to_json r | None -> `Null)
-         ; ("groups", `List (CCList.map region_group_to_json rg.children))
+  let label_path = (rg.rg_label_path |> CCList.rev |> CCList.map string_of_int |> CCString.concat ".") in
+  let label = match rg.rg_region with Some r -> "R[" ^ label_path ^ "]" | None -> "(" ^ label_path ^ ")"in
+  `Assoc [ ("constraints", `List (CCList.map (fun s -> `String s) rg.rg_constraints))
+         ; ("region", match rg.rg_region with Some r -> region_to_json r | None -> `Null)
+         ; ("groups", `List (CCList.map region_group_to_json rg.rg_children))
          ; ("label", `String label)
-         ; ("weight", `Int rg.weight)
+         ; ("weight", `Int rg.rg_weight)
          ]
 
 let regions_to_json (decompose_regions: Top_result.decompose_region list) : J.json  =
@@ -93,10 +94,10 @@ let regions_to_json (decompose_regions: Top_result.decompose_region list) : J.js
 let regions_js ft_id details_id regions = Printf.sprintf {|
 (function () {
   require(['nbextensions/nbimandra/regions'], function (regions) {
-    regions.draw('%s', %s);
+    regions.draw('%s', '%s', %s);
   });
 })();
-|} ft_id regions |> H.Unsafe.data
+|} ft_id details_id regions |> H.Unsafe.data
 
 let to_html (res : R.t) (regions: Top_result.decompose_region list) : _ html =
   Doc_render.alternatives
@@ -104,9 +105,9 @@ let to_html (res : R.t) (regions: Top_result.decompose_region list) : _ html =
      , (let uuid = (Uuidm.v `V4 |> Uuidm.to_string) in
         let ft_id = "decompose-foamtree-" ^ uuid  in
         let details_id = "decompose-details-" ^ uuid in
-        H.div ~a:[H.a_style "display: flex;"]
+        H.div ~a:[H.a_style "display: flex; justify-content: space-between;"]
           [ H.div ~a:[H.a_id ft_id; H.a_style "width: 50%; height: 400px;"] []
-          ; H.div ~a:[H.a_id details_id; H.a_style "width: 50%; height: 400px;"] []
+          ; H.div ~a:[H.a_id details_id; H.a_style "width: 45%; height: 400px;"] []
           ; H.script (regions_to_json regions |> Yojson.Basic.pretty_to_string |> regions_js ft_id details_id)
           ]
        )
