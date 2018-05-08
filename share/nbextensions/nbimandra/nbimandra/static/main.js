26fb50cb29025ef2fc68930238d08e8e938c9552
Dave Aitken <dave.aitken@gmail.com>
2018-05-08 16:47:33 +0100

    Clear button styles

--- share/nbextensions/nbimandra/nbimandra/static/main.js
+++ share/nbextensions/nbimandra/nbimandra/static/main.js
@@ -6,8 +6,10 @@ define(['jquery', 'nbextensions/nbimandra/carrotsearch.foamtree'], function ($) 
 
         //Add clear button for terminal
         if (window.terminal) {
-            $('#terminado-container').before('<div><button id="terminado-clear" class="btn btn-primary" style="margin-bottom: 1em;">Clear terminal</button></div>');
-            $('#terminado-container').on('click', '#terminado-clear', function () {
+            $('#terminado-container').css('position', 'relative');
+            $('#terminado-container').prepend('<div><button id="terminado-clear" class="btn btn-primary">Clear terminal</button></div>');
+            $('#terminado-clear').css('position', 'fixed').css('top', 100).css('right', 10).css('opacity', 0.5);
+            $('#terminado-clear').on('click', function () {
                 window.terminal.term.clear();
                 window.terminal.term.focus();
             });
