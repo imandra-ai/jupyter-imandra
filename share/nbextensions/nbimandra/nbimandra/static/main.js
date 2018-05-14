c2e6b45e96f61c90c58c5aafd68c2b40ca6d327b
Dave Aitken <dave.aitken@gmail.com>
2018-05-14 13:49:30 +0100

    Moving terminal clear button to nbimandra-theme in try notebook repo

--- share/nbextensions/nbimandra/nbimandra/static/main.js
+++ share/nbextensions/nbimandra/nbimandra/static/main.js
@@ -1,19 +1,6 @@
 define(['jquery', 'nbextensions/nbimandra/carrotsearch.foamtree'], function ($) {
 
     function load_ipython_extension () {
-
-        //Add clear button for terminal
-        if (window.terminal) {
-            $('#site').css('margin-top', 50);
-            $('#terminado-container .terminal').css('position', 'relative');
-            $('#terminado-container .terminal').prepend('<button id="terminado-clear" class="btn btn-primary">Clear terminal</button>');
-            $('#terminado-clear').css('position', 'absolute').css('top', 10).css('right', 20).css('opacity', 0.5).css('z-index', 99);
-            $('#terminado-clear').on('click', function () {
-                window.terminal.term.clear();
-                window.terminal.term.focus();
-            });
-        }
-
     }
 
     return {
