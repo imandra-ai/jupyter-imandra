e79775eefff41c1dae3ad7699f0f7d6225681928
Dave Aitken <dave.aitken@gmail.com>
2018-07-27 11:36:42 +0100

    Render rich output for instance_results

--- src/Doc_render.mli
+++ src/Doc_render.mli
@@ -10,3 +10,5 @@ val mime_of_txt : string -> Jupyter_kernel.Client.mime_data
 val alternatives : (string * [< Html_types.div_content_fun] Tyxml.Html.elt) list -> _ html
 
 val html_of_verify_result : Imandra_lib.Top_result.verify_result -> Html_types.div html
+
+val html_of_instance_result : Imandra_lib.Top_result.instance_result -> Html_types.div html
