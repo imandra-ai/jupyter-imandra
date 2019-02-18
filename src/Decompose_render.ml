d860acf7c4e15d6b8d075bb1ae30a83b2e4b00f7
Nicola Mometto <me@bronsa.net>
2019-02-18 13:39:33 +0000

    add regions_doc

--- src/Decompose_render.ml
+++ src/Decompose_render.ml
@@ -6,6 +6,13 @@ module J = Yojson.Basic
 
 type 'a html = ([> Html_types.div] as 'a) H.elt
 
+let regions_doc ?(pp_cs = (List.map Term.to_string)) regions =
+  let open Top_result in
+  let r_to_doc region = { Document.constraints = pp_cs region.reg_constraints;
+                          invariant = List.hd @@ pp_cs [region.reg_invariant] }
+  in
+  Document.regions @@ List.map r_to_doc regions
+
 let to_html ?(pp_cs = (List.map Term.to_string)) (res : Top_result.t) (decompose_regions: Top_result.decompose_region list) : _ html =
   let regions = decompose_regions |> CCList.map (Doc_render.to_region ~pp_cs) in
   Doc_render.alternatives
