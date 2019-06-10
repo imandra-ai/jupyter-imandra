8715f867f4831536299037ac9c9e79f3fbf0f4f9
Nicola Mometto <brobronsa@gmail.com>
2019-06-10 12:02:14 +0100

    fix: safe invariant printing

--- src/Decompose_render.ml
+++ src/Decompose_render.ml
@@ -8,9 +8,10 @@ type 'a html = ([> Html_types.div] as 'a) H.elt
 
 let regions_doc ?(pp_cs = (List.map Term.to_string)) regions =
   let open Top_result in
-  let r_to_doc region = { Document.constraints = pp_cs region.reg_constraints;
-                          invariant = List.hd @@ pp_cs [region.reg_invariant] }
-  in
+  let r_to_doc region =
+    let cs = pp_cs region.reg_constraints in
+    let inv = try List.hd @@ pp_cs [region.reg_invariant] with _ -> Term.to_string region.reg_invariant in
+    { Document.constraints = cs; invariant = inv } in
   Document.regions @@ List.map r_to_doc regions
 
 let to_html ?(pp_cs = (List.map Term.to_string)) (res : Top_result.t) (decompose_regions: Top_result.decompose_region list) : _ html =
